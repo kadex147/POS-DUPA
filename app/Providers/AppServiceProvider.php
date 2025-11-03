d3c5091e168bf9ef4d6b5a9164becdb183cdd50c
Kadex147 <ariawan563@gmail.com>
2025-11-04 03:39:34 +0800

    update db external n vercel 5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,25 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // <--- BARIS YANG DITAMBAHKAN
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-    // ...
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        // Set default string length untuk database
+        Schema::defaultStringLength(191);
+        
+        // Force HTTPS di production
         if ($this->app->environment('production')) {
-    URL::forceScheme('https'); // Baris ini yang paling penting
+            URL::forceScheme('https');
+            URL::forceRootUrl(config('app.url'));
         }
     }
 }
